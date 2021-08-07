5c4309364698c6da79474d3329ed5ad1eee5cfb9
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-08-08 00:45:53 +0300

    feat: add function code to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, config) {
+  for (let i = str.length - 1; i >= 0; i--) {
+    for (let item of config) str.includes(item.join('')) ? str = str.replace(item.join(''), '') : str;
+    if (!str) break;
+  }
+  return (!str);
 }
